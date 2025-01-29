e272214a68b1ef0531c905a78dffa59266e4a7b7
ThomasVanoverschelde <thomas.vano@hotmail.fr>
2025-01-29 19:42:34 +0100

    test code

---
+++ nodes/EmailCustom/EmailCustom.node.ts
@@ -0,0 +1,23 @@
+import type { INodeTypeBaseDescription, IVersionedNodeType } from 'n8n-workflow';
+import { VersionedNodeType } from 'n8n-workflow';
+
+import { EmailCustomV1 } from './v1/EmailCustomV1.node';
+
+export class EmailCustom extends VersionedNodeType {
+	constructor() {
+		const baseDescription: INodeTypeBaseDescription = {
+			displayName: 'Send Email',
+			name: 'emailCustom',
+			icon: 'fa:envelope',
+			group: ['output'],
+			defaultVersion: 1,
+			description: 'Sends an email using SMTP protocol',
+		};
+
+		const nodeVersions: IVersionedNodeType['nodeVersions'] = {
+			1: new EmailCustomV1(baseDescription)
+		};
+
+		super(nodeVersions, baseDescription);
+	}
+}
